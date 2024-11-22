dfb04e804fc5769e9e9fc28ed59370357be003cd
thanapornPit <thanaporn8793@gmail.com>
2024-11-22 06:25:51 +0000

    30

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -3,5 +3,5 @@ user_input = float(input("Enter a number less than 25:\n"))
 if user_input > 25:
     print("Error")
 else:
-for i in range(int(user_input), 26):
-    print(f"Inside the loop, my variable is {i}")
+   for i in range(int(user_input), 26):
+     print(f"Inside the loop, my variable is {i}")
